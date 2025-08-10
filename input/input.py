700ab707e1bdf79de5faef521fd6e376f9df5b51
Your Name <you@example.com>
2025-08-10 01:41:42 -0400

    Set up dataloader and basic training loop.

--- input/input.py
+++ input/input.py
@@ -1,113 +1,136 @@
+import atexit
+
+import os
+import shutil
 import numpy as np
 import torch
+from torch.utils.data import Dataset
 from tqdm import tqdm
 import pandas as pd
 from pandas import DataFrame
-from dataclasses import dataclass
 
 from common.common import CoinType, Exchange
 
 BASE_DIR = "/Users/scharlip/Desktop/crypto-predict/data/"
+WINDOWS_PER_FILE = 20000
 
-def read_csv(coin_type: CoinType, exchange: Exchange, interpolate_missing_data: bool = True) -> DataFrame:
-    csv = "{}/{}/{}USD_1m_{}.csv".format(BASE_DIR, str(coin_type).lower(), str(coin_type), str(exchange))
-    from_csv = pd.read_csv(csv, index_col="Open time")
-    from_csv['Exchange'] = str(exchange)
-    from_csv['Interpolated'] = False
+def cleanup_temp_dir(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        print(f"Temporary directory {path} cleaned up on exit.")
 
-    extraneous_columns = from_csv.columns.difference(["Open time", "Low", "High", "Open", "Close", "Volume", "Exchange", "Interpolated"])
+class CoinDataset(Dataset):
 
-    if len(extraneous_columns) > 0:
-        from_csv.drop(columns=extraneous_columns, axis=1, inplace=True)
+    def __init__(self, coin_type: CoinType, exchange: Exchange, limit=None,
+                 interpolate_missing_data: bool = True, lookback: int = 60, splits=[0.8, 0.1, 0.1]):
+        self.coin_type = coin_type
+        self.exchange = exchange
+        self.limit = limit
+        self.interpolate_missing_data = interpolate_missing_data
+        self.lookback = lookback
+        self.splits = splits
+        self.scratch_dir = "{}/scratch/".format(BASE_DIR)
+        self.midpoints_dir = "{}/midpoints".format(self.scratch_dir)
+        self.df = self.__read_csv(coin_type, exchange, interpolate_missing_data, limit)
+        self.cached_midpoints = None
 
-    from_csv.index = pd.to_datetime(from_csv.index)
+    def __read_csv(self, coin_type: CoinType, exchange: Exchange, interpolate_missing_data: bool = True, limit: int = None) -> DataFrame:
+        print("Reading csv for {}/{} ...".format(coin_type, exchange))
 
-    missing_data = pd.DataFrame(
-        {
-            "Open time": pd.date_range(
-                start=from_csv.index.min(),
-                end=from_csv.index.max(),
-                freq='min'
-            ).difference(from_csv.index)
-        }
-    )
+        csv = "{}/{}/{}USD_1m_{}.csv".format(BASE_DIR, str(coin_type).lower(), str(coin_type), str(exchange))
+        from_csv = pd.read_csv(csv, index_col="Open time")
+        from_csv['Exchange'] = str(exchange)
+        from_csv['Interpolated'] = False
 
-    missing_data = missing_data.set_index("Open time")
-    missing_data[from_csv.columns] = np.nan
-    missing_data["Interpolated"] = True
-    missing_data["Exchange"] = str(exchange)
+        extraneous_columns = from_csv.columns.difference(
+            ["Open time", "Low", "High", "Open", "Close", "Volume", "Exchange", "Interpolated"])
 
-    ret = pd.concat([from_csv, missing_data])
-    sorted = ret.sort_index()
+        if len(extraneous_columns) > 0:
+            from_csv.drop(columns=extraneous_columns, axis=1, inplace=True)
 
-    if interpolate_missing_data:
-        sorted.interpolate(inplace=True, method='linear')
+        from_csv.index = pd.to_datetime(from_csv.index)
 
-    return sorted
+        missing_data = pd.DataFrame(
+            {
+                "Open time": pd.date_range(
+                    start=from_csv.index.min(),
+                    end=from_csv.index.max(),
+                    freq='min'
+                ).difference(from_csv.index)
+            }
+        )
 
-def read_combined_csv(coin_type: CoinType) -> DataFrame:
-    combined_df = None
+        missing_data = missing_data.set_index("Open time")
+        missing_data[from_csv.columns] = np.nan
+        missing_data["Interpolated"] = True
+        missing_data["Exchange"] = str(exchange)
 
-    for exchage in tqdm(Exchange):
-        df_for_exchange = read_csv(CoinType.ETH, exchage)
+        ret = pd.concat([from_csv, missing_data])
+        sorted = ret.sort_index()
 
-        if combined_df is None:
-            combined_df = df_for_exchange
+        if interpolate_missing_data:
+            sorted.interpolate(inplace=True, method='linear')
+
+        print("Read csv for {}/{}.".format(coin_type, exchange))
+
+        if limit is not None:
+            return sorted.head(limit)
         else:
-            combined_df = pd.concat([combined_df, df_for_exchange])
+            return sorted
 
-    return combined_df.sort_index()
+    def generate_midpoint_windows(self):
+        if os.path.isdir(self.midpoints_dir):
+            print("Midpoint windows have already been extracted.")
+            return
 
-@dataclass
-class DataSet:
-    X_train: torch.tensor
-    y_train: torch.tensor
+        midpoints = (self.df["High"] + self.df["Low"]) / 2
 
-    X_validate: torch.tensor
-    y_validate: torch.tensor
+        print("Extracting midpoint windows into '{}/midpoints' ...".format(self.scratch_dir))
+        filenum = 0
+        count = 0
+        os.makedirs("{}/midpoints".format(self.scratch_dir), exist_ok=True)
+        file = open("{}/midpoints/{}.txt".format(self.scratch_dir, filenum), "w+")
+        for window in tqdm(midpoints.rolling(window=self.lookback + 1)):
+            if len(window) < self.lookback + 1:
+                continue
 
-    X_test: torch.tensor
-    y_test: torch.tensor
+            file.write(','.join(map(str, window.tolist())) + "\n")
+            count += 1
 
-def construct_midpoint_dataset(df: DataFrame, lookback: int, splits = [0.8, 0.1, 0.1]) -> DataSet:
-    df["Midpoint"] = (df["High"] + df["Low"])/2
-    dataset = df[["Midpoint"]].values.astype('float32')
+            if count % WINDOWS_PER_FILE == 0:
+                file.flush()
+                file.close()
 
-    train_size = int(len(dataset) * splits[0])
-    validate_size = int(len(dataset) * splits[1])
-    test_size = int(len(dataset) * splits[2])
+                filenum += 1
+                file = open("{}/midpoints/{}.txt".format(self.scratch_dir, filenum), "w+")
 
-    train_range = range(0, train_size)
-    validate_range = range(train_size, train_size + validate_size)
-    test_range = range(train_size + validate_size, len(dataset))
+        file.flush()
+        file.close()
+
+        print("Done extracting midpoint windows.")
+        
+    def __len__(self):
+        return len(self.df) - self.lookback
+
+    def __getitem__(self, item):
+        if item > len(self.df):
+            raise IndexError('index out of range')
+
+        file_num = int(item / WINDOWS_PER_FILE)
+
+        if self.cached_midpoints is not None and self.cached_midpoints[0] == file_num:
+            cached_windows = self.cached_midpoints[1]
+        else:
+            with open("{}/{}.txt".format(self.midpoints_dir, file_num)) as f:
+                lines = f.readlines()
+                cached_windows = [list(map(float, line.strip().split(','))) for line in lines]
+
+            self.cached_midpoints = (file_num, cached_windows)
+
+        offset = int(item % WINDOWS_PER_FILE)
+
+        window = cached_windows[offset]
+
+        return torch.tensor(window[:-1]), torch.tensor(window[-1])
 
 
-    X_train, y_train = [], []
-    for i in range(0, train_size - lookback):
-        feature = dataset[i:i + lookback]
-        target = dataset[i + 1:i + lookback + 1]
-        X_train.append(feature)
-        y_train.append(target)
-
-    X_validate, y_validate = [], []
-    for i in range(train_size - lookback, train_size + validate_size - lookback):
-        feature = dataset[i:i + lookback]
-        target = dataset[i + 1:i + lookback + 1]
-        X_validate.append(feature)
-        y_validate.append(target)
-
-    X_test, y_test = [], []
-    for i in range(train_size + validate_size - lookback, len(dataset) - lookback):
-        feature = dataset[i:i + lookback]
-        target = dataset[i + 1:i + lookback + 1]
-        X_test.append(feature)
-        y_test.append(target)
-
-    return DataSet(
-        X_train=torch.tensor(X_train),
-        y_train=torch.tensor(y_train),
-        X_validate=torch.tensor(X_validate),
-        y_validate=torch.tensor(y_validate),
-        X_test=torch.tensor(X_test),
-        y_test=torch.tensor(y_test),
-    )
--- train/train.py
+++ train/train.py
@@ -2,38 +2,60 @@ import torch.optim as optim
 import torch.utils.data as data
 import torch.nn as nn
 import torch
-import numpy as np
+from tqdm import tqdm
 
-from input.input import read_csv, CoinType, Exchange, construct_midpoint_dataset
+from common.common import CoinType, Exchange
+from input.input import CoinDataset
 from models.models import SimpleLSTMModel
 
-df = read_csv(CoinType.ETH, Exchange.Coinbase)
-dataset = construct_midpoint_dataset(df.head(1000), 10)
+ds = CoinDataset(coin_type=CoinType.ETH,exchange=Exchange.Coinbase)
+ds.generate_midpoint_windows()
+train, validate, test = torch.utils.data.random_split(ds, [0.8, 0.1, 0.1])
 
-model = SimpleLSTMModel()
+'''
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+    print("MPS backend is available and will be used.")
+else:
+    device = torch.device("cpu")
+    print("MPS backend not available, falling back to CPU.")
+'''
+
+device = torch.device("cpu")
+
+torch.set_num_threads(8)
+
+model = SimpleLSTMModel(input_size=60, device=device).to(device)
 optimizer = optim.Adam(model.parameters())
 loss_fn = nn.MSELoss()
-loader = data.DataLoader(data.TensorDataset(dataset.X_train, dataset.y_train), shuffle=True, batch_size=8)
+train_loader = data.DataLoader(train, shuffle=True, batch_size=8)
 
-n_epochs = 2000
+n_epochs = 200
 for epoch in range(n_epochs):
+    print("Starting epoch {} ...".format(epoch))
     model.train()
-    for X_batch, y_batch in loader:
+    for X_batch, y_batch in tqdm(train_loader):
+        X_batch = X_batch.to(device)
+        y_batch = y_batch.to(device)
+
         y_pred = model(X_batch)
+        y_pred = y_pred.squeeze()
         loss = loss_fn(y_pred, y_batch)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-    # Validation
-    if epoch % 100 != 0:
-        continue
+    print("Done with epoch {}.".format(epoch))
 
-    model.eval()
+    #TODO: run cross validation
 
+# TODO: run test
+
+'''
     with torch.no_grad():
         y_pred = model(dataset.X_train)
         train_rmse = np.sqrt(loss_fn(y_pred, dataset.y_train))
         y_pred = model(dataset.X_test)
         test_rmse = np.sqrt(loss_fn(y_pred, dataset.y_test))
     print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
+'''
