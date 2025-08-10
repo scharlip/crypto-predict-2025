9bdbdb33c16a396d603f0c9352a3476026ac2f57
Your Name <you@example.com>
2025-08-10 02:36:16 -0400

    Set up simple validation.

--- train/train.py
+++ train/train.py
@@ -3,6 +3,8 @@ import torch.utils.data as data
 import torch.nn as nn
 import torch
 from tqdm import tqdm
+import numpy as np
+import statistics
 
 from common.common import CoinType, Exchange
 from input.input import CoinDataset
@@ -10,7 +12,7 @@ from models.models import SimpleLSTMModel
 
 ds = CoinDataset(coin_type=CoinType.ETH,exchange=Exchange.Coinbase)
 ds.generate_midpoint_windows()
-train, validate, test = torch.utils.data.random_split(ds, [0.8, 0.1, 0.1])
+train, validate, test = torch.utils.data.random_split(ds, [0.8, 0.05, 0.15])
 
 '''
 if torch.backends.mps.is_available():
@@ -28,11 +30,17 @@ torch.set_num_threads(8)
 model = SimpleLSTMModel(input_size=60, device=device).to(device)
 optimizer = optim.Adam(model.parameters())
 loss_fn = nn.MSELoss()
-train_loader = data.DataLoader(train, shuffle=True, batch_size=8)
+train_loader = data.DataLoader(train, shuffle=True, batch_size=32)
+validation_loader = data.DataLoader(validate, shuffle=True, batch_size=32)
 
 n_epochs = 200
+
 for epoch in range(n_epochs):
     print("Starting epoch {} ...".format(epoch))
+
+    print("Starting training for epoch {} ...".format(epoch))
+
+    errors = []
     model.train()
     for X_batch, y_batch in tqdm(train_loader):
         X_batch = X_batch.to(device)
@@ -45,17 +53,27 @@ for epoch in range(n_epochs):
         loss.backward()
         optimizer.step()
 
+    print("Completed training for epoch {}.".format(epoch))
+
+    errors = []
+
+    print("Starting validation for epoch {} ...".format(epoch))
+
+    model.eval()
+    with torch.no_grad():
+        for X_batch, y_batch in tqdm(validation_loader):
+            X_batch = X_batch.to(device)
+            y_batch = y_batch.to(device)
+
+            y_pred = model(X_batch)
+            y_pred = y_pred.squeeze()
+            rmse = np.sqrt(loss_fn(y_pred, y_batch))
+            errors.append(rmse)
+
+    avg_error = statistics.fmean(errors)
+
+    print("Completed validation for epoch {}. Avg RMSE: {}".format(epoch, avg_error))
+
     print("Done with epoch {}.".format(epoch))
 
-    #TODO: run cross validation
-
 # TODO: run test
-
-'''
-    with torch.no_grad():
-        y_pred = model(dataset.X_train)
-        train_rmse = np.sqrt(loss_fn(y_pred, dataset.y_train))
-        y_pred = model(dataset.X_test)
-        test_rmse = np.sqrt(loss_fn(y_pred, dataset.y_test))
-    print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
-'''
