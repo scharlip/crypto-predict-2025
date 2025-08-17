579d30037154e547f5c97f63d7b5ea8d5da68100
Your Name <you@example.com>
2025-08-17 17:00:08 -0400

    Clean up perfect model.

--- models/PerfectMidpointPredictorModel.py
+++ models/PerfectMidpointPredictorModel.py
@@ -9,26 +9,19 @@ from models.base import BaseModel, TransctionType
 
 class PerfectMidpointPredictorModel(BaseModel):
 
-    def __init__(self, ds: MidpointCoinDataset, threshold: float, lookahead: int):
+    def __init__(self, ds: MidpointCoinDataset, threshold: float, lookahead: int) -> List[float]:
         self.df = ds.df
         self.threshold = threshold
         self.lookahead = lookahead
+        self.cache = None
 
-    def __predict_future_window(self, past_window: DataFrame) -> List[float]:
-        '''
-        with_noise = []
-        for v in perfect_prediction:
-            random.seed(v)
-            with_noise.append(v * random.uniform(1.0 - artificial_noise_pctg, 1.0 + artificial_noise_pctg))
-        '''
-
-
+    def __predict_future_window(self, past_window: DataFrame) -> DataFrame:
         end_timestamp = past_window.iloc[-1]["Open time"]
         end_index = self.df[self.df["Open time"] == end_timestamp].index.tolist()[0]
 
         window_df = self.df[end_index + 1 : end_index + self.lookahead + 1]
 
-        return window_df["Midpoint"].tolist()
+        return window_df
 
     def buy_sell_hold_decision(
             self,
@@ -37,32 +30,25 @@ class PerfectMidpointPredictorModel(BaseModel):
             last_purchased_price: float,
             currently_have_usd: bool) -> Tuple[TransctionType, int]:
 
+        # TODO: cache the window by current_time so that these scans go faster
         future_window = self.__predict_future_window(past_window)
-
-        min_value = float('inf')
-        min_index = -1
-        max_value = float('-inf')
-        max_index = -1
-
-        for (idx, val) in enumerate(future_window):
-            if val > max_value:
-                max_value = val
-                max_index = idx
-
-            if val < min_value:
-                min_value = val
-                min_index = idx
+        min_idx = future_window["Midpoint"].idxmin()
+        max_idx = future_window["Midpoint"].idxmax()
+        min_value = future_window.loc[min_idx]["Midpoint"].tolist()
+        max_value = future_window.loc[max_idx]["Midpoint"].tolist()
+        min_datetime = future_window.loc[min_idx]["Open time"].to_pydatetime()
+        max_datetime = future_window.loc[max_idx]["Open time"].to_pydatetime()
 
         if currently_have_usd:
             # buy at the predicted minimum if the maximum is more than the minimum + the threshold
-            if max_value > min_value * (1 + self.threshold) and max_index > min_index:
-                return (TransctionType.Buy, current_time + timedelta(minutes = min_index))
+            if max_value > min_value * (1 + self.threshold) and max_idx > min_idx:
+                return (TransctionType.Buy, min_datetime)
             else:
                 return (TransctionType.Hold, None)
         else:
             # if we predict a value above the threshold, buy there
             if max_value > last_purchased_price * (1 + self.threshold):
-                return (TransctionType.Sell, current_time + timedelta(minutes = max_index))
+                return (TransctionType.Sell, max_datetime)
 
             # if we predict a value 3x below the threshold, bail out right now
             elif min_value < last_purchased_price * (1 - 3 * self.threshold):
