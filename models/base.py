f613e5eb9fb304c51b405ec3bc530da92ebc3941
Your Name <you@example.com>
2025-08-17 17:52:33 -0400

    Clean up comments.

--- models/base.py
+++ models/base.py
@@ -13,9 +13,6 @@ class TransctionType(StrEnum):
 
 class BaseModel(nn.Module):
 
-    #def predict_future_window(self, current_idx: int, past_window: List[float], lookahead: int) -> List[int]:
-    #    raise NotImplementedError()
-
     def buy_sell_hold_decision(
             self,
             current_time: datetime,
