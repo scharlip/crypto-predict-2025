c7ea0f6d71d3f18be1985225f0a7e6db4df76995
Your Name <you@example.com>
2025-08-18 22:57:39 -0400

    Remove commented out caching logic.

--- models/MidpointModelPredictor.py
+++ models/MidpointModelPredictor.py
@@ -1,20 +1,9 @@
-from dataclasses import dataclass
-from datetime import datetime, timedelta
+from datetime import datetime
 from typing import Tuple
 
 from pandas import DataFrame
 
 from models.BaseModel import BaseModel, TransctionType
-
-@dataclass
-class CacheEntry:
-    current_time: datetime
-
-    min_idx: int
-    min_value: float
-
-    max_idx: int
-    max_value: float
 
 class MidpointPredictorModel(BaseModel):
 
@@ -31,54 +20,6 @@ class MidpointPredictorModel(BaseModel):
                    future_window: DataFrame,
                    last_purchased_price: float,
                    currently_have_usd: bool) -> Tuple[TransctionType, int]:
-
-        # caching logic is below. doesn't appear to speed things up much though
-
-        '''
-        # try to use cache entry if it's present and was for the previous minute
-        if self.cache is not None and current_time - self.cache.current_time == timedelta(minutes = 1):
-            cached = self.cache
-            trailing_point = future_window.iloc[-1]["Midpoint"].tolist()
-
-            min_value = trailing_point if trailing_point < cached.min_value else  cached.min_value
-            min_idx = future_window.index[-1] if trailing_point < cached.min_value else cached.min_idx
-
-            max_value = trailing_point if trailing_point > cached.max_value else  cached.max_value
-            max_idx = future_window.index[-1] if trailing_point > cached.max_value else cached.max_idx
-
-            # if the min index has fallen outside the index range, recalculate it
-            if min_idx < future_window.index[0]:
-                min_idx = future_window["Midpoint"].idxmin()
-                min_value = future_window.loc[min_idx]["Midpoint"].tolist()
-
-            # if the max index has fallen outside the index range, recalculate it
-            if max_idx < future_window.index[0]:
-                max_idx = future_window["Midpoint"].idxmax()
-                max_value = future_window.loc[max_idx]["Midpoint"].tolist()
-
-            self.cache = CacheEntry(
-                current_time=current_time,
-                min_idx=min_idx,
-                min_value=min_value,
-                max_idx=max_idx,
-                max_value=max_value
-            )
-        # otherwise recalculate
-        else:
-            min_idx = future_window["Midpoint"].idxmin()
-            max_idx = future_window["Midpoint"].idxmax()
-            min_value = future_window.loc[min_idx]["Midpoint"].tolist()
-            max_value = future_window.loc[max_idx]["Midpoint"].tolist()
-
-            self.cache = CacheEntry(
-                current_time=current_time,
-                min_idx=min_idx,
-                min_value=min_value,
-                max_idx=max_idx,
-                max_value=max_value
-            )
-        '''
-
         min_idx = future_window["Midpoint"].idxmin()
         max_idx = future_window["Midpoint"].idxmax()
         min_value = future_window.loc[min_idx]["Midpoint"].tolist()
